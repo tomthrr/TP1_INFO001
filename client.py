08ce7a7c751b5e4e779792edce6de6d7595c088f
Tom <73913043+tomthrr@users.noreply.github.com>
2023-11-14 16:23:28 +0100

    aaaaa

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import ssl
 
 # Configuration du contexte SSL
 context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
-context.load_verify_locations('nginx-reverse/ssl/serveur_http.pem')
+context.load_verify_locations('serveur_http.pem')
 
 # Création du socket SSL
 client_socket = socket.create_connection(('localhost', 443))
