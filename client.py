872855ad14b3b83671cb2f6ee195725ce3e944f8
Tom <73913043+tomthrr@users.noreply.github.com>
2023-11-14 16:29:34 +0100

    bbbb

--- client.py
+++ client.py
@@ -1,17 +1,33 @@
 import socket
 import ssl
 
+# Paramètres du client
+HOST = 'www.thierry.fr'  # Adresse IP du serveur
+PORT = 12345         # Port d'écoute du serveur
+CERTFILE = 'serveur_http.cert.pem'  # Chemin vers le certificat du client
+KEYFILE = 'serveur_http.pem'   # Chemin vers la clé privée du client
+
+# Création du socket client SSL
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 # Configuration du contexte SSL
 context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
-context.load_verify_locations('serveur_http.pem')
+context.load_cert_chain(certfile=CERTFILE, keyfile=KEYFILE)
 
-# Création du socket SSL
-client_socket = socket.create_connection(('localhost', 443))
-ssl_socket = context.wrap_socket(client_socket, server_hostname='localhost')
+# Connexion au serveur
+secure_client_socket = context.wrap_socket(client_socket, server_hostname=HOST)
+secure_client_socket.connect((HOST, PORT))
 
-# Envoi d'un message au serveur
-message = "Bonjour depuis le client!"
-ssl_socket.send(message.encode())
+print(f"Connecté au serveur {HOST}:{PORT}")
+
+while True:
+    # Envoi de données au serveur
+    message = input("Client: ")
+    secure_client_socket.send(message.encode('utf-8'))
+
+    # Attente de la réponse du serveur
+    data = secure_client_socket.recv(1024).decode('utf-8')
+    print(f"Serveur: {data}")
 
 # Fermeture de la connexion
-ssl_socket.close()
+secure_client_socket.close()
--- server.py
+++ server.py
@@ -1,31 +1,43 @@
 import socket
 import ssl
 
-# Chemin vers les fichiers de certificat et de clé privée
-certfile = 'serveur_http.cert.pem'
-keyfile = 'serveur_http.pem'
+# Paramètres du serveur
+HOST = '127.0.0.1'  # Adresse IP du serveur
+PORT = 12345         # Port d'écoute du serveur
+CERTFILE = 'serveur_http.cert.pem'  # Chemin vers le certificat du client
+KEYFILE = 'serveur_http.pem'   # Chemin vers la clé privée du client
+
+# Création du socket serveur SSL
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((HOST, PORT))
+server_socket.listen()
 
 # Configuration du contexte SSL
 context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-context.load_cert_chain(certfile=certfile, keyfile=keyfile)
+context.load_cert_chain(certfile=CERTFILE, keyfile=KEYFILE)
 
-# Création du socket SSL
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('localhost', 443))
-server_socket.listen(1)
+print(f"Le serveur écoute sur {HOST}:{PORT}")
 
-# Attente de connexion
-print("En attente de connexion...")
-client_socket, addr = server_socket.accept()
+# Attente de la connexion d'un client
+client_socket, client_address = server_socket.accept()
+print(f"Connexion établie avec {client_address}")
 
-# Handshake SSL
-ssl_socket = context.wrap_socket(client_socket, server_side=True)
-print(f"Connexion établie avec {addr}")
+# Wrapping du socket dans le contexte SSL
+secure_client_socket = context.wrap_socket(client_socket, server_side=True)
 
-# Réception et affichage du message du client
-data = ssl_socket.recv(1024)
-print(f"Message reçu du client : {data.decode()}")
+while True:
+    # Attente de données depuis le client
+    data = secure_client_socket.recv(1024).decode('utf-8')
+
+    if not data:
+        break
+
+    print(f"Client: {data}")
+
+    # Réponse au client
+    message = input("Serveur: ")
+    secure_client_socket.send(message.encode('utf-8'))
 
 # Fermeture des connexions
-ssl_socket.close()
+secure_client_socket.close()
 server_socket.close()
