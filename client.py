d20d30bdf0d6d209d8cb8e92b00e362eb028c902
Tom <73913043+tomthrr@users.noreply.github.com>
2023-11-15 09:26:49 +0100

    fix: last fix, add print

--- client.py
+++ client.py
@@ -1,17 +1,21 @@
 import socket
 import ssl
 
+HOSTNAME = 'www.thierry.fr'
+SERVER_HOSTNAME = 'www.thierry.fr'
+PORT = 8080
+
 # Création d'une socket TCP/IP
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connexion au serveur
-client_socket.connect(('www.thierry.fr', 8080))
+client_socket.connect((HOSTNAME, PORT))
 
 # Configuration du contexte SSL
 ssl_context = ssl.create_default_context()
 
 # Utilisation du contexte SSL pour sécuriser la connexion
-secure_client_socket = ssl_context.wrap_socket(client_socket, server_hostname='www.thierry.fr')
+secure_client_socket = ssl_context.wrap_socket(client_socket, server_hostname=SERVER_HOSTNAME)
 
 # Envoi de données au serveur
 message = "Message du client : Bonjour serveur !"
@@ -20,6 +24,16 @@ secure_client_socket.sendall(message.encode())
 # Réception des données du serveur
 data = secure_client_socket.recv(1024)
 print(f"Données reçues du serveur : {data.decode()}")
+
+# Récupération du certificat du serveur
+server_cert = secure_client_socket.getpeercert()
+
+# Affichage des informations du certificat
+print("Informations du certificat reçu :")
+print(f"Émetteur (issuer): {server_cert['issuer']}\n")
+
+if (SERVER_HOSTNAME in server_cert['subjectAltName'][0][1]):
+    print("Le certificat est valide pour le domaine", SERVER_HOSTNAME)  
 
 # Fermeture de la connexion
 secure_client_socket.close()
